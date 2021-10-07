5c6686e9159fbd69ea30d2e0893534c55b35d4ab
lilmissrayna <lilmissrayna@gmail.com>
2021-10-06 21:01:12 -0500

    finished services and added encryption/decryption

--- src/main/java/com/revature/service/EmployeeServiceImpl.java
+++ src/main/java/com/revature/service/EmployeeServiceImpl.java
@@ -1,57 +1,98 @@
 package com.revature.service;
 
 import java.util.List;
-
 import com.revature.models.ReimbursementStatus;
 import com.revature.models.ReimbursementType;
 import com.revature.models.Request;
 import com.revature.models.User;
 import com.revature.repo.RequestDAO;
 import com.revature.repo.UserDAO;
+import java.util.Base64;
 
 public class EmployeeServiceImpl implements EmployeeService {
 
 	private RequestDAO rDao;
 	private UserDAO uDao;
-	
+
 	public EmployeeServiceImpl(UserDAO uDao, RequestDAO rDao) {
 		this.uDao = uDao;
 		this.rDao = rDao;
 	}
-	
+
 	@Override
 	public User login(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return uDao.selectUser(username);
 	}
 
 	@Override
 	public boolean authenticate(String username, String password) {
-		// TODO Auto-generated method stub
-		return false;
+
+		boolean authenticated = false;
+		List<User> allUsers = uDao.selectAllUsers();
+
+		for (User user : allUsers) {
+			if (user.getUsername().equals(username)) {
+				if (decryptPassword(user.getPassword()).equals(password)) {
+					authenticated = true;
+					break;
+				} else {
+					// incorrect password
+					authenticated = false;
+					break;
+				}
+			}
+		}
+		return authenticated;
 	}
-	
+
 	@Override
 	public boolean checkIfUsernameExists(String username) {
-		// TODO Auto-generated method stub
-		return false;
+
+		boolean usernameExists = false;
+
+		List<User> allUsers = uDao.selectAllUsers();
+
+		for (User user : allUsers) {
+			if (user.getUsername().equals(username)) {
+				usernameExists = true;
+				break;
+			}
+		}
+
+		return usernameExists;
 	}
 
 	@Override
 	public boolean requestReimbursment(String username, ReimbursementType type, double amount, String description) {
-		// TODO Auto-generated method stub
-		return false;
+		return rDao.insertRequest(username, type, amount, description);
 	}
 
 	@Override
 	public List<Request> viewPendingRequests(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return rDao.selectRequestByUsernameAndStatus(username, ReimbursementStatus.PENDING);
 	}
 
 	@Override
 	public List<Request> viewPastRequests(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return rDao.selectPastRequestByUsername(username);
+	}
+
+	@Override
+	public boolean createNewAccount(String username, String email) {
+		return uDao.insertUser(username, email);
+	}
+
+	@Override
+	public boolean changePassword(String username, String newPassword) {
+		String encryptedPassword = encryptPassword(newPassword);
+		return uDao.updatePassword(username, encryptedPassword);
+	}
+
+	private String encryptPassword(String password) {
+		return Base64.getEncoder().encode(password.getBytes()).toString();
+	}
+	
+	private String decryptPassword(String password) {
+		return Base64.getDecoder().decode(password.getBytes()).toString();
 	}
 }
