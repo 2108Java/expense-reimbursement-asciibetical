7a825ee161d1371b2b77418980fce9ca9451e14a
lilmissrayna <lilmissrayna@gmail.com>
2021-10-07 17:22:46 -0500

    Finished service layer

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,19 +1,8 @@
 package com.revature;
-import com.revature.repo.RequestDAO;
-import com.revature.repo.RequestDAOImpl;
-import com.revature.repo.UserDAO;
-import com.revature.repo.UserDAOImpl;
-import com.revature.service.EmployeeServiceImpl;
+
 public class MainDriver {
 
 	public static void main(String[] args) {
-		UserDAO uDao = new UserDAOImpl();
-		RequestDAO rDao = new RequestDAOImpl();
-		EmployeeServiceImpl test = new EmployeeServiceImpl(uDao, rDao);
-		System.out.println("hello");
-		test.createNewAccount("user4", "lilmissrayna@gmail.com");
-		
-
 	}
 
 }
