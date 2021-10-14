c2595aa3fc6a62c13e694f0fb2fbb12b7d7071db
lilmissrayna <lilmissrayna@gmail.com>
2021-10-13 23:08:16 -0500

    added toggles

--- target/classes/public/dashboardJS.js
+++ target/classes/public/dashboardJS.js
@@ -1,26 +1,12 @@
-let makeRequest = document.getElementById("make-request");
-let viewPast = document.getElementById("view-past");
-let viewPending = document.getElementById("view-pending");
-let viewAll = document.getElementById("view-all");
-let updateStatus = document.getElementById("update-status");
 let financeManagerDiv = document.getElementById("finance-manager-options");
 
-window.addEventListener('load', startup);
-window.addEventListener('load', on);
+window.addEventListener('load',startup);
 
-function startup() {
-    if (4 > 2) {    // if user is a finance manager
+function startup(){
+    if(4>2){    // if user is a finance manager
         financeManagerDiv.style.display = "block";
-    } else {
+    }else{
         financeManagerDiv.style.display = "none";
     }
 
-}
-
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
-    document.getElementById("overlay").style.display = "none";
 }
